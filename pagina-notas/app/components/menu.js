7da119d2c00f7d08c3f6374368c23a9f594ad5b7
Seqenenra <juandavidalvarez08@gmail.com>
2023-06-08 09:36:27 -0500

    menu

---
+++ pagina-notas/app/components/menu.js
@@ -0,0 +1,13 @@
+export default function Menu() {
+    return (
+    <nav>
+        <ul>
+            <li><a href="#">Crear Materia</a></li>
+            <li><a href="#">Ver Notas</a></li>
+            <li><a href="#">Editar Materia</a></li>
+            <li><a href="#">Instrucciones</a></li>
+            <li><a href="#">Imagen</a></li>
+        </ul>
+    </nav>
+    )
+  }
--- pagina-notas/app/page.js
+++ pagina-notas/app/page.js
@@ -1,10 +1,11 @@
 import Image from 'next/image'
+import Menu from './components/menu'
 import styles from './page.module.css'
 
 export default function Home() {
   return (
     <body className={styles.main}>
-      <h1 className={styles.description}>hola como está</h1>
+      <Menu></Menu>
     </body>
   )
 }
