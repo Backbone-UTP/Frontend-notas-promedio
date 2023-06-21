d46edccf4de1cb8ac913c895e7c4e8cf7912fd18
Seqenenra <juandavidalvarez08@gmail.com>
2023-06-21 17:06:04 -0500

    instrucciones-con-footer-mal-hecho

--- pagina-notas/app/page.js
+++ pagina-notas/app/page.js
@@ -1,10 +1,15 @@
 import Menu from './components/menu'
+import Image from "next/image"
+import logoBackBone from "../public/logoBackBone.svg"
+import Instrucciones from './components/instrucciones'
 import './style/page.css'
 
 export default function Home() {
   return (
     <body>
       <Menu></Menu>
+      <Instrucciones></Instrucciones>
+      <footer><Image className="imagen_logo_backbone" src={logoBackBone} alt="Logo backbone" /></footer>
     </body>
   )
 }
