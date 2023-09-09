17a48b4c947ff377cfe00898b1ac152ace32f83a
seqenenra08 <juandavidalvarez08@gmail.com>
2023-09-08 19:51:21 -0500

    Cambios del objeto en cada archivo, y accion de agregar lo ingresado por el usuario al objeto

--- pagina-notas/app/components/editarNotas.js
+++ pagina-notas/app/components/editarNotas.js
@@ -1,189 +1,24 @@
 "use client"
 import { useState } from "react"
 import "../style/editarNotas.css"
-
-
-const data = [
-        {
-            materia: "matematicas IV", 
-            id: 1,
-            cortes: [
-                {
-                    id: "1",
-                    porcentaje: "30",
-                    notas: [
-                        {
-                            id: "1",
-                            nota: "4.5",
-                            porcentaje: "30",
-                        },
-                        {
-                            id: "2",
-                            nota: "4.5",
-                            porcentaje: "30",
-                        },
-                        {
-                            id: "3",
-                            nota: "4.5",
-                            porcentaje: "40",
-                        }
-                        
-                    ], 
-                },
-                {
-                    id: "2",
-                    porcentaje: "30",
-                    notas: [
-                        {
-                            id: "1",
-                            nota: "4.5",
-                            porcentaje: "30",
-                        },
-                        {
-                            id: "2",
-                            nota: "4.5",
-                            porcentaje: "30",
-                        },
-                        {
-                            id: "3",
-                            nota: "4.5",
-                            porcentaje: "40",
-                        }
-                        
-                    ], 
-                },
-                {
-                    id: "3",
-                    porcentaje: "40",
-                    notas: [
-                        {
-                            id: "1",
-                            nota: "4.5",
-                            porcentaje: "30",
-                        },
-                        {
-                            id: "2",
-                            nota: "4.5",
-                            porcentaje: "30",
-                        },
-                        {
-                            id: "3",
-                            nota: "4.5",
-                            porcentaje: "40",
-                        }
-                        
-                    ], 
-                },
-            ]
-        },
-        {
-            materia: "fisica II", 
-            id: 2,
-            cortes: [
-                {
-                    id: "1",
-                    porcentaje: "30",
-                    notas: [
-                        {
-                            id: "1",
-                            nota: "4.5",
-                            porcentaje: "30",
-                        },
-                        {
-                            id: "2",
-                            nota: "4.5",
-                            porcentaje: "30",
-                        },
-                        {
-                            id: "3",
-                            nota: "4.5",
-                            porcentaje: "40",
-                        }
-                        
-                    ], 
-                },
-                {
-                    id: "2",
-                    porcentaje: "30",
-                    notas: [
-                        {
-                            id: "1",
-                            nota: "4.5",
-                            porcentaje: "30",
-                        },
-                        {
-                            id: "2",
-                            nota: "4.5",
-                            porcentaje: "30",
-                        },
-                        {
-                            id: "3",
-                            nota: "4.5",
-                            porcentaje: "40",
-                        }
-                        
-                    ], 
-                },
-                {
-                    id: "3",
-                    porcentaje: "40",
-                    notas: [
-                        {
-                            id: "1",
-                            nota: "4.5",
-                            porcentaje: "30",
-                        },
-                        {
-                            id: "2",
-                            nota: "4.5",
-                            porcentaje: "30",
-                        },
-                        {
-                            id: "3",
-                            nota: "4.5",
-                            porcentaje: "40",
-                        }
-                        
-                    ], 
-                },
-            ]
-        },
-        {
-            materia:"Introduccion a la bareta I",
-            id: 3,
-            cortes: [
-                {
-                    id: "1",
-                    porcentaje: "100",
-                    notas: [
-                        {
-                            id: "1",
-                            nota: "6.9",
-                            porcentaje: "100",
-                        },
-                        {
-                            id: "2",
-                            nota: "5.0",
-                            porcentaje: "10",
-                        }
-                    ],
-                }
-            ]
-        },
-    ];
+import { data } from "./varibaleDatos";
 
 export default function EditarNotas(){
 
-    const [nombreMateria, setNombreMateria] = useState('');
-    const [selectedTab, setSelectedTab] = useState(0);
-    const [idCorte, setIdCorte] = useState(-1);
-    const [idMateria, setIdMateria] = useState(-1);
+    const [nombreMateria, setNombreMateria] = useState('')
+    const [selectedTab, setSelectedTab] = useState(0)
+    const [idCorte, setIdCorte] = useState(-1)
+    const [idMateria, setIdMateria] = useState(-1)
     const [botonElegido, setBotonElegido] = useState(1)
 
-    const [selectedCreditos, setCreditos] = useState('')
-    const [selectedObtenido, setObtenido] = useState('')
-    const [selectedEsperado, setEsperado] = useState('')
-    const [nombreCorte, setNombreCorte] = useState('')
+    const [nameCut,setNameCut] = useState('')
+    const [percentageCut,setPercentageCut] = useState('')
+    const [obtainedCut,setObtainedCut] = useState('')
+    const [expectedCut,setExpectedCut] = useState('')
+
+    const[nameGrade,setNameGrade] = useState('')
+    const[percentageGrade,setPercentageGrade] = useState('')
+    const[obtainedGrade,setObtainedGrade] = useState('')
 
     const [valorcheckbox, setvalorcheckbox] = useState(false)
 
@@ -202,6 +37,7 @@ export default function EditarNotas(){
 
                 <div className="contenedor_botones_inferiores">
                     <button className="boton_editar" onClick={() => idCorte != -1 ? (setSelectedTab(2), setBotonElegido(3)) : alert("No has seleccionado ningun corte")}>Editar Nota</button>
+                    <button className="boton_editar" onClick={() => idCorte != -1 ? (setSelectedTab(2) , setBotonElegido(4)) : alert("No has seleccionado ningun corte")}>Crear Nota</button>
                 </div>
             </div>)
         }else if(botonElegido == 1){
@@ -209,21 +45,21 @@ export default function EditarNotas(){
             <form>
                 <div>
                     <div>Nombre Corte</div>
-                    <input required type="text" onChange={e => setNombreCorte(e.target.value)}></input>
+                    <input required type="text" onChange={e => setNameCut(e.target.value)}></input>
                 </div>
                 <div>
                     <div>Porcentaje</div>
-                    <input required type="number" max="100" min="0" className="number" value={selectedCreditos} onChange={e => (parseFloat(e.target.value) > 0 && parseFloat(e.target.value) <= 100 && (parseFloat(e.target.value) - parseInt(e.target.value))== 0) ? setCreditos(e.target.value): setCreditos('')}></input>
+                    <input required type="number" max="100" min="0" className="number" value={percentageCut} onChange={e => (parseFloat(e.target.value) > 0 && parseFloat(e.target.value) <= 100 && (parseFloat(e.target.value) - parseInt(e.target.value))== 0) ? setPercentageCut(e.target.value): setPercentageCut('')}></input>
                 </div>
                 <div>
                     <div>Obtenido</div>
-                    <input required type="number" max="5" min="0" className="number" value={selectedObtenido} onChange={e => (parseFloat(e.target.value) >= 0 && parseFloat(e.target.value) <= 5) ? setObtenido(e.target.value): setObtenido('')}></input>
+                    <input required type="number" max="5" min="0" className="number" value={obtainedCut} onChange={e => (parseFloat(e.target.value) >= 0 && parseFloat(e.target.value) <= 5) ? setObtainedCut(e.target.value): setObtainedCut('')}></input>
                             </div>
                 <div>
                     <div>Esperado</div>
-                    <input required type="number" max="5" min="0" className="number" value={selectedEsperado} onChange={e => (parseFloat(e.target.value) >= 0 && parseFloat(e.target.value) <= 5) ? setEsperado(e.target.value): setEsperado('')}></input>
+                    <input required type="number" max="5" min="0" className="number" value={expectedCut} onChange={e => (parseFloat(e.target.value) >= 0 && parseFloat(e.target.value) <= 5) ? setExpectedCut(e.target.value): setExpectedCut('')}></input>
 
-                    <input type="button" onClick={() => (nombreCorte != '' && selectedCreditos != '' && selectedObtenido != '' && selectedEsperado != '') ? (setSelectedTab(selectedTab + 1), setNombreCorte(''),  setCreditos(''),  setObtenido(''),  setEsperado('')) : alert("NO VE QUE NO HA LLENADO EL CAMPO AGUEVADO")} className="enviar" value="Crear Nota"></input>
+                    <input type="button" onClick={() => (nameCut != '' && percentageCut != '' && obtainedCut != '' && expectedCut != '') ? setSelectedTab(selectedTab + 1) : alert("NO VE QUE NO HA LLENADO EL CAMPO AGUEVADO")} className="enviar" value="Crear Nota"></input>
                 </div>
             </form>)
         }
@@ -237,17 +73,17 @@ export default function EditarNotas(){
             <form>
                 <div>
                     <div>Nombre Nota</div>
-                    <input required type="text"  onChange={e => setNombreCorte(e.target.value)}></input>
+                    <input required type="text"  onChange={e => setNameGrade(e.target.value)}></input>
                 </div>
                 <div>
                     <div>Porcentaje</div>
-                    <input required type="number" max="100" min="0" className="number" value={selectedCreditos} onChange={e => (parseFloat(e.target.value) > 0 && parseFloat(e.target.value) <= 100 && (parseFloat(e.target.value) - parseInt(e.target.value))== 0) ? setCreditos(e.target.value): setCreditos('')}></input>
+                    <input required type="number" max="100" min="0" className="number" value={percentageGrade} onChange={e => (parseFloat(e.target.value) > 0 && parseFloat(e.target.value) <= 100 && (parseFloat(e.target.value) - parseInt(e.target.value))== 0) ? setPercentageGrade(e.target.value): setPercentageGrade('')}></input>
                 </div>
                 <div>
                     <div>Obtenido</div>
-                    <input required type="number" max="5" min="0" className="number" value={selectedObtenido}  onChange={e => (parseFloat(e.target.value) >= 0 && parseFloat(e.target.value) <= 5) ? setObtenido(e.target.value): setObtenido('')}></input>
+                    <input required type="number" max="5" min="0" className="number" value={obtainedGrade}  onChange={e => (parseFloat(e.target.value) >= 0 && parseFloat(e.target.value) <= 5) ? setObtainedGrade(e.target.value): setObtainedGrade('')}></input>
 
-                    <input type="button" onClick={() => (nombreCorte != '' && selectedCreditos != '' && selectedObtenido != '') ? (setNombreCorte(''),  setCreditos(''),  setObtenido(''),  setEsperado(''), window.location.href="/ver-notas") : alert("NO VE QUE NO HA LLENADO EL CAMPO AGUEVADO")} className="enviar" value="Crear Materia"></input>
+                    <input type="button" onClick={() => (nameGrade != '' && percentageGrade != '' && obtainedGrade != '') ? (chooseToAdd(), window.location.href="/ver-notas")  : alert("NO VE QUE NO HA LLENADO EL CAMPO AGUEVADO")} className="enviar" value="Crear Materia"></input>
                 </div>
             </form>)
         }
@@ -292,6 +128,51 @@ export default function EditarNotas(){
         }
     }
 
+    const chooseToAdd = () => {
+        if(botonElegido == 4){
+            addNoteToObject()
+        }else{
+            addToObject()
+        }
+    }
+
+    const addToObject = () => {
+        let idLastCut = data[idMateria].cortes.length + 1
+
+        data[idMateria].cortes.push(
+            {
+                corte: nameCut,
+                id: idLastCut.toString(),
+                porcentaje: percentageCut,
+                obtenido: obtainedCut,
+                esperado: expectedCut,
+                notas: [
+                    {
+                        name: nameGrade,
+                        id: "1",
+                        porcentaje: percentageGrade,
+                        nota: obtainedGrade,
+                    },  
+                ], 
+            }
+        )
+        console.log(data)
+    }
+
+    const addNoteToObject = () => {
+        let idLastNote = data[idMateria].cortes[idCorte].notas.length + 1
+
+        data[idMateria].cortes[idCorte].notas.push(
+            {
+                name: nameGrade,
+                id: idLastNote.toString(),
+                porcentaje: percentageGrade,
+                nota: obtainedGrade,
+            }
+        )
+        console.log(data)
+    }
+
     return(
         
         <div className="contenedor_universal">
@@ -311,7 +192,7 @@ export default function EditarNotas(){
                     <div className="contenedor_botones_inferiores">
                         <button className="boton_editar" onClick={() => idMateria != -1 ? (setSelectedTab(1), setBotonElegido(0)) : alert("No has seleccionado ninguna materia")}>Editar Corte</button>
 
-                        <button className="boton_crear" onClick={() => idMateria != -1 ? (setSelectedTab(1), setBotonElegido(1)) : alert("No has seleccionado ninguna materia")}>Crear Corte</button>
+                        <button className="boton_editar" onClick={() => idMateria != -1 ? (setSelectedTab(1), setBotonElegido(1)) : alert("No has seleccionado ninguna materia")}>Crear Corte</button>
                     </div>
                 </div>
             </div>
