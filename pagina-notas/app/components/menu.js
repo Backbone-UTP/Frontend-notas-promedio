bc1a578ceefcf9959a3c90c2dd4f9ca8c6653d31
Juan Alvarez <juandavidalvarez08@gmail.com>
2023-10-12 17:28:05 -0500

    image classname change

--- pagina-notas/app/components/menu.js
+++ pagina-notas/app/components/menu.js
@@ -13,14 +13,14 @@ export default function Menu() {
             <span className={btnMenu == 0 ? 'line2_btn_menu' : 'line2_btn_menu activeline2_btn_menu'}></span>
             <span className={btnMenu == 0 ? 'line3_btn_menu' : 'line3_btn_menu activeline3_btn_menu'}></span>
         </div>
-        <div className="contenedor_imagen"><Image className="imagen_logo_utp" src={logoUtp} alt="Logo utp" /></div>
+        <div className="image"><Image src={logoUtp} alt="Logo utp" /></div>
         <nav className={btnMenu == 0 ? 'nav_menu' : 'nav_menu shownav_menu'}>
             <ul className="nav_menu_lista">
                 <li><a href="/crear-materia">Crear Materia</a></li>
                 <li><a href="/ver-notas">Ver Notas</a></li>
                 <li><a href="/editar-materia">Editar Materia</a></li>
                 <li><a href="/">Instrucciones</a></li>
-                <li className="contenedor_imagen"><Image className="imagen_logo_utp" src={logoUtp} alt="Logo utp" /></li>
+                <li className="image"><Image src={logoUtp} alt="Logo utp" /></li>
             </ul>
         </nav>
     </header>
