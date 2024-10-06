3c569946e771166a16ae855b2037118d3b7dc5ef
seqenenra08 <juandavidalvarez08@gmail.com>
2024-10-05 20:50:26 -0500

    Improve responsiveness of card design

--- src/app/Components/carousel.js
+++ src/app/Components/carousel.js
@@ -19,18 +19,18 @@ export default function Carousel() {
   const title = Object.keys(carts);
 
   return (
-    <div className="relative min-h-[550px] min-w-[825px] overflow-hidden flex justify-center">
+    <div className="relative min-h-[600px] min-w-[825px] overflow-hidden flex justify-center max-sm:min-w-[300px] max-sm:min-h-[610px] max-[768px]:min-w-[470px] max-[768px]:min-h-[600px]">
       <Card index={0} currentIndex={currentIndex} values={carts[title[0]]} main={true}/>
       {title.map((t, index) => (
         <Card key={t} index={index + 1} currentIndex={currentIndex} values={carts[t]} main={false}/>
       ))}
       <button 
-        className="absolute bottom-0 left-0 transform -translate-y-1/2 bg-white bg-opacity-50 text-black px-2 py-1 rounded-full"
+        className="absolute bottom-0 left-[25%] transform -translate-y-1/2 bg-white bg-opacity-50 text-black px-2 py-1 rounded-full max-[768px]:left-0"
         onClick={handlePrev}>
         {"<"}
       </button>
       <button 
-        className="absolute bottom-0 right-0 transform -translate-y-1/2 bg-white bg-opacity-50 text-black px-2 py-1 rounded-full"
+        className="absolute bottom-0 right-[25%] transform -translate-y-1/2 bg-white bg-opacity-50 text-black px-2 py-1 rounded-full max-sm max-[768px]:right-0"
         onClick={handleNext}>
         {">"}
       </button>
--- src/app/page.js
+++ src/app/page.js
@@ -3,8 +3,8 @@ import Image from 'next/image';
 
 export default function Home() {
   return (<>
-    <main className="min-w-full min-h-screen flex justify-center items-center">
-      <Image src="/jirafa2.png" alt="logo" layout="fill" objectFit="contain"  className="absolute z-[-50] bottom-0 opacity-30"/>
+    <main className="relative min-w-full min-h-screen flex justify-center items-center">
+      <Image src="/jirafa2.png" alt="logo" fill priority className=" z-[-50] bottom-0 opacity-30 object-contain max-sm:hidden"/>
       <Carousel />
     </main>
   </>
